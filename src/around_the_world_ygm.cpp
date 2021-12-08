904f9f9afd78ab3358d02e2ba03ba9876c2aa5ea
Trevor Steil <trevor.steil1@gmail.com>
2021-12-07 16:26:20 -0800

    Adds initial YGM around_the_world benchmark

--- src/around_the_world_ygm.cpp
+++ src/around_the_world_ygm.cpp
@@ -1,8 +1,45 @@
+// Copyright 2019-2021 Lawrence Livermore National Security, LLC and other YGM
+// Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: MIT
 
 #include <ygm/comm.hpp>
+#include <ygm/utility.hpp>
+
+static int num_trips;
+static int curr_trip = 0;
+
+struct around_the_world_functor {
+ public:
+  template <typename Comm>
+  void operator()(Comm *world) {
+    if (curr_trip < num_trips) {
+      world->async((world->rank() + 1) % world->size(),
+                   around_the_world_functor());
+      ++curr_trip;
+    }
+  }
+};
 
 int main(int argc, char **argv) {
   ygm::comm world(&argc, &argv);
+
+  num_trips = atoi(argv[1]);
+
+  world.barrier();
+
+  ygm::timer trip_timer{};
+
+  if (world.rank0()) {
+    world.async(1, around_the_world_functor());
+  }
+
+  world.barrier();
+
+  double elapsed = trip_timer.elapsed();
+
+  world.cout0("Went around the world ", num_trips, " times in ", elapsed,
+              " seconds\nAverage trip time: ", elapsed / num_trips);
 
   return 0;
 }

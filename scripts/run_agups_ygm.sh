d11a7888b72ec169295e903ce1259286cb8a1baa
Trevor Steil <trevor.steil1@gmail.com>
2022-03-07 14:51:25 -0800

    Removes unused variables from scripts

--- scripts/run_agups_ygm.sh
+++ scripts/run_agups_ygm.sh
@@ -3,10 +3,9 @@
 YGM_BENCH_BUFFER_CAPACITIES=(16777216 33554432 67108864)
 YGM_ROUTING_PROTOCOLS=("YGM_ROUTING_PROTOCOL_DIRECT" "YGM_ROUTING_PROTOCOL_NR")
 
-global_log_table_sizes=(24 25 26 27)
 global_log_table_size=$(echo ${SLURM_JOB_NUM_NODES} | python3 -c "import math; print(24+round(math.log2(float(input()))))")
 local_updaters=100000
-updater_lifetime=100
+updater_lifetime=10
 num_trials=10
 
 for routing_protocol in "${YGM_ROUTING_PROTOCOLS[@]}"; do

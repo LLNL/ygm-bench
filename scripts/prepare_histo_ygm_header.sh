8bfc1095b13d99d45a08d383e07592e242a462a9
Trevor Steil <trevor.steil1@gmail.com>
2023-03-31 22:53:53 -0700

    Removes unused files

--- scripts/prepare_histo_ygm_header.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-export YGM_BENCH_HISTO_OUTPUT_FILE="${YGM_BENCH_OUTPUT_DIR}/histo_ygm.out"
-
-date >> ${YGM_BENCH_HISTO_OUTPUT_FILE}
-echo "Nodes, Ranks per Node, Buffer Capacity, Routing Protocol, Global Histogram Size, Global Insertions, Trial Time, Trial Insertions/Sec (Billions)" \
-	>> ${YGM_BENCH_HISTO_OUTPUT_FILE}
